d92d46ad78dcee92e6821a3e264e181f91680af9
alien-house <info@alien-house.com>
2017-01-01 15:42:52 -0800

    finished one course

--- AngularJS/udemy/js/app.js
+++ AngularJS/udemy/js/app.js
@@ -20,14 +20,37 @@ app.config(function($routeProvider,$locationProvider){
 	$locationProvider.hashPrefix('');
 });
 
-app.service("GroceryService",function(){
+app.service("GroceryService",function($http){
 
 	var groceryService = {};
 	groceryService.groceryItems = [
-		{id: 1, completed: true, itemName: 'milk', data: '2014-10-21'},
-		{id: 2, completed: true, itemName: 'cookies', data: '2014-10-01'},
-		{id: 3, completed: true, itemName: 'der', data: '2014-10-09'}
+		// {id: 1, completed: true, itemName: 'milk', data: '2014-10-21'},
+		// {id: 2, completed: true, itemName: 'cookies', data: '2014-10-01'},
+		// {id: 3, completed: true, itemName: 'der', data: '2014-10-09'}
 	];
+
+	// $http.get("http://example.com/api/");
+	$http({
+		method: 'GET',
+		url: 'data/server_data.json'
+	}).then(function successCallback(response) {
+		groceryService.groceryItems = response.data;
+		for(var item in groceryService.groceryItems){
+			// console.log(groceryService.groceryItems[item]);
+			groceryService.groceryItems[item].date = new Date(groceryService.groceryItems[item].date);
+			console.log(groceryService.groceryItems[item].date);
+		}
+	}, function errorCallback(data, status) {
+		alert("Things went wrong");
+	});
+
+
+	// $http.get("data/server_data.json")
+	// 	.success(function(data){
+	// 	})
+	// 	.error(function(data, status){
+
+	// 	});
 
 	groceryService.findById = function(id){
 		for(var item in groceryService.groceryItems){
@@ -36,8 +59,7 @@ app.service("GroceryService",function(){
 		}
 	};
 
-
-
+	// If you use DB, you dont need this function.
 	groceryService.getNewId = function(){
 		if(groceryService.newId){
 			groceryService.newId++;
@@ -49,14 +71,46 @@ app.service("GroceryService",function(){
 		}
 	};
 
+	groceryService.markCompleted = function(entry){
+		entry.completed = !entry.completed;
+	}
+
+	groceryService.removeItem = function(entry){
+		$http.post("data/delete_item.json", {id:entry.id})
+			.success(function(data){
+				if(data.status){
+					var index = groceryService.groceryItems.indexOf(entry);
+					groceryService.groceryItems.splice(index, 1);
+				}
+			})
+			.error(function(data, status){
+			})
+
+	}
+
 	groceryService.save = function(entry){
 		var updateItem = groceryService.findById(entry.id);
 		if(updateItem){
-			updateItem.completed = entry.completed;
-			updateItem.itemName = entry.itemName;
-			updateItem.date = entry.date;
+			$http.post("data/updated_item.json", entry)
+			.success(function(data){
+				if(data.status == 1){
+					updateItem.completed = entry.completed;
+					updateItem.itemName = entry.itemName;
+					updateItem.date = entry.date;
+				}
+			})
+			.error(function(data, status){
+			})
+
 		}else{
-			entry.id = groceryService.getNewId();
+			$http.post("data/added_item.json", entry)
+			.success(function(data){
+				entry.id = data.newId;
+			})
+			.error(function(data, status){
+			})
+
+			// entry.id = groceryService.getNewId();
 			groceryService.groceryItems.push(entry);
 		}
 	}
@@ -67,6 +121,18 @@ app.controller("HomeController", ["$scope","GroceryService", function($scope, Gr
 	// $scope.appTitle = GroceryService.groceryItems[0].itemName;
 	$scope.appTitle = "Grocery List";
 	$scope.groceryItems = GroceryService.groceryItems;
+	$scope.removeItem = function(entry){
+		GroceryService.removeItem(entry);
+	};
+	$scope.markCompleted = function(entry){
+		GroceryService.markCompleted(entry);
+	};
+	// when something in "GroceryService.groceryItems" has changed, it update it.
+	$scope.$watch(function(){
+		return GroceryService.groceryItems;
+	}, function(groceryItems){
+		$scope.groceryItems = groceryItems;
+	});
 
 }]);
 
@@ -82,9 +148,15 @@ app.controller("GroceryListItemsController", ["$scope", "$routeParams", "$locati
 		GroceryService.save($scope.groceryItem);
 		$location.path("/");
 	}
-	console.log($scope.groceryItem);
+	// console.log($scope.groceryItem);
 }]);
 
+app.directive("tbGroceryItem", function(){
+	return{
+		restrict: "E",
+		templateUrl: "views/groceryItem.html",
+	}
+})
 
 
 // // var app = angular.module('tutorialApp', ["tutorialCtrlModule"]);

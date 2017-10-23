816235461ed414b46880dfd72cc06c2e568a504a
alien-house <info@alien-house.com>
2017-10-22 17:07:29 -0700

    add forloop

---
+++ typescript/essentialtraining_todo/app_forloop.ts
@@ -0,0 +1,16 @@
+
+
+var array = [
+    "Pick up drycleaning",
+    "Clean Batcave",
+    "Save Gotham"
+];
+
+for (var index in array) {
+    var value = array[index];
+    console.log(`${index}: ${value}`);
+}
+
+for (var value of array) {
+    console.log(`${value}`);
+}
